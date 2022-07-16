a760982b48937531c079dd412f64baf26f876234
denisbilous1989 <denisbilous1989@gmail.com>
2022-07-16 20:19:55 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = 10;
+
+while (a <= 20) {
+  let result = a;
+  document.write(result + ', ');
+  a = a + 1;
+}
+
+document.write("<br>");
+
+let b = 20;
+
+while (b >= 10) {
+  let result = b;
+  document.write(result + ', ');
+  b = b - 1;
+}
